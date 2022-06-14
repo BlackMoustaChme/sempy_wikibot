e8c8ab046ae71bba86034ffdc4c69d82bc5d5c36
BlackMoustaChe <chme@live.ru>
2022-06-14 08:19:05 +0700

    SemPyPL_Wikibot upd4

--- sempy_wikibot/wiki_bot.py
+++ sempy_wikibot/wiki_bot.py
@@ -5,14 +5,15 @@ import datetime as dt
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, User, Update, \
     Message
 import wikipedia
-from telegram.ext import Updater, CommandHandler, Filters, CallbackQueryHandler, MessageHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, filters, CallbackQueryHandler, MessageHandler, CallbackContext, Application
 from tokens import token
+# from telegram.ext.
 
 # import keyboards.disambiguation_keyboard as dkb
 # import config
 
 # Messages
-
+# bot = telebot.TeleBot(token.TG_TOKEN)
 lang = "en"
 count = 1
 checklist_eng = {'Eng', 'eng', 'ENG', 'EN', 'en', 'English', 'english', 'En', 'англ', 'Англ', 'Английский',
@@ -29,18 +30,19 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
-def create_workbook(user_id):
-    workbook = xlsxwriter.Workbook(f'{user_id}.xlsx')
-    worksheet = workbook.add_worksheet()
-
-    worksheet.write(0, 0, "Дата")
-    worksheet.write(0, 1, "Время")
-    worksheet.write(0, 2, "Вид сообщения")
-    worksheet.write(0, 3, "Отправитель")
-    worksheet.write(0, 4, "ID отправителя")
-    worksheet.write(0, 5, "Сообщение")
-
-    return worksheet
+# def create_workbook(user_id):
+#     workbook = xlsxwriter.Workbook(f'messages.xlsx')
+#     worksheet = workbook.add_worksheet()
+#
+#     worksheet.write(0, 0, "Дата")
+#     worksheet.write(0, 1, "Время")
+#     worksheet.write(0, 2, "Вид сообщения")
+#     worksheet.write(0, 3, "Отправитель")
+#     worksheet.write(0, 4, "ID отправителя")
+#     worksheet.write(0, 5, "Сообщение")
+#     print("work")
+#
+#     return worksheet
 
 
 def create_disambiguation_keyboard(disambiguation_options):
@@ -53,23 +55,23 @@ def create_disambiguation_keyboard(disambiguation_options):
     return InlineKeyboardMarkup(keyboard)
 
 
-def start(update, context):
-    update.message.reply_text('Hi there! \U0001F60A \n\n' + HELP_MESSAGE)
+async def start(update, context):
+    await update.message.reply_text('Hi there! \U0001F60A \n\n' + HELP_MESSAGE)
     # Message.from_user(User.id)
 
 
-def help(update, context):
-    update.message.reply_text('Sending help now \U0001F60A \n\n' + HELP_MESSAGE)
+async def help(update, context):
+    await update.message.reply_text('Sending help now \U0001F60A \n\n' + HELP_MESSAGE)
 
 
-def get_lang(update, context):
+async def get_lang(update, context):
     global lang
     if lang == "en":
-        update.message.reply_text('Current language: English')
+        await update.message.reply_text('Current language: English')
     # elif lang == "":
     #     update.message.reply_text('Current language: English')
     elif lang == "ru":
-        update.message.reply_text('Текущий язык: Русский')
+        await update.message.reply_text('Текущий язык: Русский')
 
 
 # async def set_lang(update, context):
@@ -113,7 +115,7 @@ def get_lang(update, context):
 #
 #     await query.edit_message_text(text=f"Selected language: {query.data}")
 
-def set_lang(update, context):
+async def set_lang(update, context):
     global lang
     global checklist_eng
     global checklist_ru
@@ -121,24 +123,24 @@ def set_lang(update, context):
     rus_lang_button = KeyboardButton(text='Rus', )
     keyboard_array = [[eng_lang_button, rus_lang_button]]
     reply_markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True, keyboard=keyboard_array)
-    update.message.reply_text('Choose language for searching', reply_markup=reply_markup)
+    await update.message.reply_text('Choose language for searching', reply_markup=reply_markup)
 
 
-def eng(update: Update, context: CallbackContext):
+async def eng(update: Update, context: CallbackContext):
     global lang
     # user_input = query.data
     lang = "en"
-    update.message.reply_text("Selected language: Eng")
+    await update.message.reply_text("Selected language: Eng")
 
 
-def rus(update: Update, context: CallbackContext):
+async def rus(update: Update, context: CallbackContext):
     global lang
     # user_input = query.data
     lang = "ru"
-    update.message.reply_text("Selected language: Rus")
+    await update.message.reply_text("Selected language: Rus")
 
 
-def callback_handler(update, context):
+async def callback_handler(update, context):
     query = update.callback_query
     callback_data = query.data
 
@@ -150,10 +152,10 @@ def callback_handler(update, context):
             message = FIND_OUT_MORE + page_result.url
             query.edit_message_text(text=summary + message)
     except:
-        query.edit_message_text(text="Whut? An unprecedented error has occurred.")
+        await query.edit_message_text(text="Whut? An unprecedented error has occurred.")
 
 
-def wiki(update, context):
+async def wiki(update, context):
     global lang
     wikipedia.set_lang(lang)
     # print(wikipedia.languages())
@@ -165,28 +167,28 @@ def wiki(update, context):
         update.message.reply_text(summary + message)
     except wikipedia.exceptions.DisambiguationError as disambiguation:
         try:
-            update.message.reply_text('Please choose:',
+            await update.message.reply_text('Please choose:',
                                       reply_markup=create_disambiguation_keyboard(disambiguation.options))
         except:
-            update.message.reply_text("There is disambiguity but an unprecedented error has occurred.")
+            await update.message.reply_text("There is disambiguity but an unprecedented error has occurred.")
 
     except wikipedia.exceptions.PageError as page_error:
-        update.message.reply_text("Unable to find wiki page for \'" + user_input + "\'.")
+        await update.message.reply_text("Unable to find wiki page for \'" + user_input + "\'.")
 
     except wikipedia.exceptions.HTTPTimeoutError as http_timeout:
-        update.message.reply_text("Time out! Mediawiki is giving me trouble...")
+        await update.message.reply_text("Time out! Mediawiki is giving me trouble...")
 
     except wikipedia.exceptions.RedirectError as redirect_error:
-        update.message.reply_text("Baam! Redirection Error.")
+        await update.message.reply_text("Baam! Redirection Error.")
 
     except wikipedia.exceptions.WikipediaException as wiki_exception:
-        update.message.reply_text("Welp... Wiki base class error.")
+        await update.message.reply_text("Welp... Wiki base class error.")
 
     except:
-        update.message.reply_text("Whut? An unprecedented error has occurred.")
+        await update.message.reply_text("Whut? An unprecedented error has occurred.")
 
 
-def random_wiki(update, context):
+async def random_wiki(update, context):
     global lang
     wikipedia.set_lang(lang)
     user_input = wikipedia.random(pages=1)
@@ -197,85 +199,108 @@ def random_wiki(update, context):
         update.message.reply_text(summary + message)
     except wikipedia.exceptions.DisambiguationError as disambiguation:
         try:
-            update.message.reply_text('Please choose:',
+            await update.message.reply_text('Please choose:',
                                       reply_markup=create_disambiguation_keyboard(disambiguation.options))
         except:
-            update.message.reply_text("There is disambiguity but an unprecedented error has occurred.")
+            await update.message.reply_text("There is disambiguity but an unprecedented error has occurred.")
 
     except wikipedia.exceptions.PageError as page_error:
-        update.message.reply_text("Unable to find wiki page for \'" + user_input + "\'.")
+        await update.message.reply_text("Unable to find wiki page for \'" + user_input + "\'.")
 
     except wikipedia.exceptions.HTTPTimeoutError as http_timeout:
-        update.message.reply_text("Time out! Mediawiki is giving me trouble...")
+        await update.message.reply_text("Time out! Mediawiki is giving me trouble...")
 
     except wikipedia.exceptions.RedirectError as redirect_error:
-        update.message.reply_text("Baam! Redirection Error.")
+        await update.message.reply_text("Baam! Redirection Error.")
 
     except wikipedia.exceptions.WikipediaException as wiki_exception:
-        update.message.reply_text("Welp... Wiki base class error.")
+        await update.message.reply_text("Welp... Wiki base class error.")
 
     except:
-        update.message.reply_text("Whut? An unprecedented error has occurred.")
+        await update.message.reply_text("Whut? An unprecedented error has occurred.")
 
 
 def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
-def log(update: Update, context):
-    print("Hi")
-    print(Message.from_user)
+# @bot.message_handler(content_types=['text'])
+async def log(update, context):
     global count
-    worksheet = create_workbook(Message.from_user)
     print("Hi")
-    worksheet.write(count, 0, str(dt.datetime.now().date()))
-    worksheet.write(count, 1, str(dt.datetime.now().time()[0:0]))
-    worksheet.write(count, 2, "Текст")
-    worksheet.write(count, 3, Message.from_user(User.first_name) + '  ' + Message.from_user(User.last_name))
-    worksheet.write(count, 4, Message.from_user(User.id))
-    worksheet.write(count, 5, update.message.text)
-    count += 1
+    print(update.message.chat.id)
+    # print(update.message.text)
+    log_file = f'{update.message.chat.id}.log'
+    with open(log_file, "at+") as file:
+        file.write(update.message.text + '\n')
+    print("Hi")
+
+    # workbook = xlsxwriter.Workbook(f'{update.message.chat.id}.xlsx')
+    # worksheet = workbook.add_worksheet()
+
+    # worksheet.write(0, 0, "Дата")
+    # worksheet.write(0, 1, "Время")
+    # worksheet.write(0, 2, "Вид сообщения")
+    # worksheet.write(0, 3, "Отправитель")
+    # worksheet.write(0, 4, "ID отправителя")
+    # worksheet.write(0, 5, "Сообщение")
+
+    # worksheet = create_workbook(update.message.chat.id)
+    # if update.message.text != 'стоп':
+    #     worksheet.write(count, 0, str(dt.datetime.now().date()))
+    #     worksheet.write(count, 1, str(dt.datetime.now().time())[0:0])
+    #     worksheet.write(count, 2, "Текст")
+    #     worksheet.write(count, 3, update.message.chat.first_name + '  ' + update.message.chat.last_name)
+    #     worksheet.write(count, 4, update.message.chat.id)
+    #     worksheet.write(count, 5, update.message.text)
+    #     count += 1
+        # await update.message.reply_text(update.message.text)
+    # workbook.close()
+    print("Hi")
 
 
 def main():
-    updater = Updater(token.TG_TOKEN, use_context=True)
-
+    # updater = Updater(token.TG_TOKEN, use_context=True)
+    app = Application.builder().token(token.TG_TOKEN).build()
     '''Deployment'''
     # updater.start_webhook(listen="0.0.0.0", port=config.PORT, url_path=config.TOKEN)
 
-    dp = updater.dispatcher
-
-    dp.add_handler(MessageHandler(Filters.regex(re.compile(r'start', re.IGNORECASE)), start))
-    dp.add_handler(MessageHandler(Filters.regex(re.compile(r'help', re.IGNORECASE)), help))
+    # dp = updater.dispatcher
+    app.add_handler(MessageHandler(filters.TEXT & filters.COMMAND, log))
+    app.add_handler(MessageHandler(filters.Regex(re.compile(r'start', re.IGNORECASE)), start))
+    app.add_handler(MessageHandler(filters.Regex(re.compile(r'help', re.IGNORECASE)), help))
     # dp.add_handler(MessageHandler(Filters.regex(re.compile(r'wiki', re.IGNORECASE)), wiki))
-    dp.add_handler(MessageHandler(Filters.regex(re.compile(r'random_wiki', re.IGNORECASE)), random_wiki))
-    dp.add_handler(MessageHandler(Filters.regex(re.compile(r'getlang', re.IGNORECASE)), get_lang))
-    dp.add_handler(MessageHandler(Filters.regex(re.compile(r'setlang', re.IGNORECASE)), set_lang))
-    dp.add_handler(MessageHandler(Filters.regex("Eng"), eng))
-    dp.add_handler(MessageHandler(Filters.regex("Rus"), rus))
-    dp.add_handler(MessageHandler(Filters.regex("log"), log))
+    app.add_handler(MessageHandler(filters.Regex(re.compile(r'random_wiki', re.IGNORECASE)), random_wiki))
+    app.add_handler(MessageHandler(filters.Regex(re.compile(r'getlang', re.IGNORECASE)), get_lang))
+    app.add_handler(MessageHandler(filters.Regex(re.compile(r'setlang', re.IGNORECASE)), set_lang))
+    app.add_handler(MessageHandler(filters.Regex("Eng"), eng))
+    app.add_handler(MessageHandler(filters.Regex("Rus"), rus))
+
 
     '''Old ver'''
     # dp.add_handler(CommandHandler("start", start))
     # dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("wiki", wiki))
+    app.add_handler(CommandHandler("wiki", wiki))
     # dp.add_handler(CommandHandler("random_wiki", random_wiki))
     # dp.add_handler(CommandHandler("getlang", get_lang))
     # dp.add_handler(CommandHandler("setlang", set_lang))
     # dp.add_handler(CommandHandler("Eng", eng))
     # dp.add_handler(CommandHandler("Rus", rus))
 
-    updater.dispatcher.add_handler(CallbackQueryHandler(callback_handler))
+    # app.updater.dispatcher.add_handler(CallbackQueryHandler(callback_handler))
+    # app.add_handler(CallbackQueryHandler(callback_handler))
 
-    dp.add_error_handler(error)
+    app.add_error_handler(error)
 
     '''Deployment'''
     # updater.bot.set_webhook(config.APP_URL + config.TOKEN)
 
     '''Development'''
-    updater.start_polling()
+    # updater.start_polling()
+    app.run_polling()
+    # bot.polling(none_stop=True)
 
-    updater.idle()
+    app.idle()
 
     # print('Hi')
 
@@ -287,5 +312,4 @@ def main():
 if __name__ == '__main__':
     print("Starting bot...")
     main()
-
     print("Bot stopped.")
